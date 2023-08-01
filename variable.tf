ff849d570568e4ad88f654bdb9344806a32daf18
Grant Moore <chinchalinchin@gmail.com>
2023-08-01 10:18:32 -0400

    remove cognito var

--- variable.tf
+++ variable.tf
@@ -2,8 +2,3 @@ variable "namespace" {
     type                = string
     description         = "Namespace of the application"
 }
-
-variable "cognito_audiences" {
-    type                = list(string)
-    description         = "List of Cognito audiences for the Trust Relationship in the Tenant Assume Role policy."
-}
